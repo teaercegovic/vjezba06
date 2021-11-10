a28f2743f854b30126855fc781dfbc73f518620d
teaercegovic <77160442+teaercegovic@users.noreply.github.com>
2021-11-10 17:20:36 +0100

    promjena adrese url na heroku

--- okviri_p06_frontend/src/services/poruke.js
+++ okviri_p06_frontend/src/services/poruke.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+const osnovniUrl = 'https://vj06.herokuapp.com/api/poruke'
  
 const dohvatiSve = () => {   
     return axios.get(osnovniUrl);
